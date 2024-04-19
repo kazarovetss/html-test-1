6ac30ce078ee3aea337a3c0d1ed49772ff5681d1
UserHub <6694489kaz@mail.ru>
2024-04-19 15:47:06 +0300

    date

--- .idea/admin_window.php
+++ .idea/admin_window.php
@@ -1,5 +1,7 @@
 <?php
 $header = file_get_contents("html/header.html");
 $body = file_get_contents("html/body-admin.html");
+$date = file_get_contents("html/date.html");
+$body = str_replace("{DATE}", $date, $body);
 echo $header;
 echo $body;
